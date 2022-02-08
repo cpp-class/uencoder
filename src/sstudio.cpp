1dd4f787e0cadd131920540df98729bdf9d510fe
abd <abd.manian@gmail.com>
2022-02-09 00:17:36 +0330

    Ask for user name

--- src/sstudio.cpp
+++ src/sstudio.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <string>
+
 
 int main() {
+//	std::cout << " ### String Studio #### \n";
 
-	std::cout << " ### String Studio #### \n";
+	std::string first_name;
+	std::string family_name;
+	int age;
+
+	std::cout << "Please enter your name+age:";
+	std::cin >> first_name >> family_name >> age ;
+
+	std::cout << "Hello " << first_name << "-" << family_name <<". Your age is " << age << "!\n";
+
 }
