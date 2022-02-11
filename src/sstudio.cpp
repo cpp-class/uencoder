007dd17f530bce5ef6437b49551eedcd6f5f6087
abd <abd.manian@gmail.com>
2022-02-11 11:25:06 +0330

    Add char table

--- src/sstudio.cpp
+++ src/sstudio.cpp
@@ -1,17 +1,39 @@
 #include <iostream>
 #include <string>
 
+void print_all_charachters()
+{
+	std::cout << "Print all charachters: \n";
+	for (int cnt = 0; cnt < 255; ++cnt) {
+		char x = cnt;
+
+		std::cout << "Char [" << cnt << "] = " << x << "\n";
+	}
+}
+
+void print_some_escape_chars()
+{
+	std::string my_string {"Escape sequences: \" \' \? \n \\ \r \t Some text \x5c" "  "};
+
+	std::cout << my_string << "\n";
+}
 
 int main() {
-//	std::cout << " ### String Studio #### \n";
 
-	std::string first_name;
-	std::string family_name;
-	int age;
+	std::cout << "Please select target function: \n"
+			" a : Print all chars\n"
+			" b : Print escape sequences \n"
+			":";
+	std::string func_name;
+	std::cin >> func_name;
 
-	std::cout << "Please enter your name+age:";
-	std::cin >> first_name >> family_name >> age ;
+	if (func_name == "a")
+	{
+		print_all_charachters();
+	}
 
-	std::cout << "Hello " << first_name << "-" << family_name <<". Your age is " << age << "!\n";
-
+	if (func_name == "b")
+	{
+		print_some_escape_chars();
+	}
 }
